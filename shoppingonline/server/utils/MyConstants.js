87f4b601ff00c592c2213adb519f5e4139d1d4b4
thanhphuuu <phupham0911@gmail.com>
2024-10-21 11:26:02 +0700

    DOAN

---
+++ shoppingonline/server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster1.qsgyh.mongodb.net',
+    DB_USER: 'phu',
+    DB_PASS: '123',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '123',
+    JWT_EXPIRES: '3600000', // in milliseconds
+    EMAIL_USER: 'phudsdsds111@gmail.com', // gmail service
+    EMAIL_PASS: 'phudsdsds'
+  };
+  module.exports = MyConstants;
